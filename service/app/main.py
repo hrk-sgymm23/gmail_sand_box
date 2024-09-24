ddc9046d0474976dab8b805553fc626cdbf71973
hrk-sgymm23 <b1805473@planet.kanazawa-it.ac.jp>
2024-09-24 10:39:28 +0900

    feat: add firebase emulators

--- service/app/main.py
+++ service/app/main.py
@@ -5,8 +5,20 @@ import google.cloud.firestore
 
 
 app = Flask(__name__)
-dbname = os.environ['FIRESTORE_DB_NAME']
-db = google.cloud.firestore.Client(database=dbname)
+emulator_host = os.getenv('FIRESTORE_EMULATOR_HOST')
+
+
+print(f"{emulator_host=}")
+if emulator_host:
+    print(f"Connecting to Firestore emulator at {emulator_host}")
+    project = os.getenv('GCLOUD_PROJECT')
+    db = google.cloud.firestore.Client(project=project)
+else:
+    # 本番の場合は実際のDB名を指定する
+    print("Connecting to Firestore production environment")
+    dbname = os.environ['FIRESTORE_DB_NAME']
+    db = google.cloud.firestore.Client(database=dbname)
+
 
 @app.route("/", methods=["POST"])
 def index():
@@ -24,15 +36,15 @@ def index():
 
     pubsub_message = envelope["message"]
 
+    results = db.collection('human').stream()
+    data_list = [doc.to_dict() for doc in results]
+
+    print(data_list)
+
     name = "World"
     if isinstance(pubsub_message, dict) and "data" in pubsub_message:
         name = base64.b64decode(pubsub_message["data"]).decode("utf-8").strip()
 
     print(f"Hello {name}!")
-
-    results = db.collection('human').stream()
-    data_list = [doc.to_dict() for doc in results]
-
-    print(data_list)
 
     return ("", 204)
